6fb8d67eca2dd69d4901870997e4639d282d40f1
Yury Petrov <jpsbur@gmail.com>
2015-12-07 18:16:21 +0300

    submit-stats: frozen submissions

--- submit-stats/submit-stats.py
+++ submit-stats/submit-stats.py
@@ -46,10 +46,13 @@ def gen_problem_submit_stats(contest_data):
   for p in problem_submits:
     cnt_ac = [0] * slices
     cnt_rj = [0] * slices
+    cnt_fz = [0] * slices
     for s in problem_submits[p]:
       t = int(s[3]) // slice_size
       if s[4] == 'OK':
         cnt_ac[t] += 1
+      elif s[4] == 'FZ':
+        cnt_fz[t] += 1
       else:
         cnt_rj[t] += 1
 
@@ -58,6 +61,7 @@ def gen_problem_submit_stats(contest_data):
     line_chart.x_labels = [str(i * slice_size) for i in range(slices)]
     line_chart.add('AC', cnt_ac)
     line_chart.add('RJ', cnt_rj)
+    line_chart.add('FZ', cnt_fz)
     f = open(p + '.stats.svg', 'wb')
     f.write(line_chart.render())
     f.close()
